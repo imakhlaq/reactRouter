8d31bc165a56d7a2d5163c322fb5e02e343a4822
imakhlaq <imakhlaqXD@gmail.com>
2022-12-27 14:53:44 +0530

    react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuotes from "./pages/NewQuotes";
+import QuoteDetails from "./pages/QuoteDetails";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteID">
+        <QuoteDetails />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuotes />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/pages/QuoteDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+const QuoteDetails = () => {
+
+    //this will give :part with custom url
+  const params = useParams();
+  return <div>QuoteDetails</div>;
+};
+
+export default QuoteDetails;
